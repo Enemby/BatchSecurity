5a3118768d7952b193934d63208416ca3d715417
Enemby <Enemby@users.noreply.github.com>
2014-02-27 23:37:21 -0700

    Update CaughtYou.bat
    I accidentally didn't upload the whole file.

--- CaughtYou.bat
+++ CaughtYou.bat
@@ -2,4 +2,4 @@ title Caught you!
 echo You aren't supposed to be here, looking at my files. I know you've been here now.
 COLOR 0a
 echo %date% %time% %computername% %username% >> \\documents\2014\user\Programs\Mine\Access.txt
-IF NOT  '%username%' == 'user' echo %date%
+IF NOT  '%username%' == 'user' echo %date% %time% %computername% %username% >> \\documents\2014\user\Access.txt
