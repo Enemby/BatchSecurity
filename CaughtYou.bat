b99f2df34465ef01938b2a72f43b45e6fd70e674
Enemby <Enemby@users.noreply.github.com>
2015-01-21 22:00:12 -0700

    Cleaned up CaughtYou.bat

--- CaughtYou.bat
+++ CaughtYou.bat
@@ -1,5 +1,6 @@
+@echo off
+IF NOT '%username%' == 'Me' echo %date% %time% %computername% %username% >> Access.txt
 title Caught you!
 echo You aren't supposed to be here, looking at my files. I know you've been here now.
 COLOR 0a
-echo %date% %time% %computername% %username% >> \\documents\2014\user\Programs\Mine\Access.txt
-IF NOT  '%username%' == 'user' echo %date% %time% %computername% %username% >> \\documents\2014\user\Access.txt
+pause
