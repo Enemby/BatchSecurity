d21daa3b8b54e3fe38901758bb00f45ee03326fd
Enemby <Enemby@users.noreply.github.com>
2016-06-29 15:08:42 -0600

    Modified messages to be a bit more subtle
    As in, maybe don't tell the user we're logging them?

--- CaughtYou.bat
+++ CaughtYou.bat
@@ -1,6 +1,6 @@
 @echo off
 IF NOT '%username%' == 'Me' echo %date% %time% %computername% %username% >> Access.txt
-title Caught you!
-echo You aren't supposed to be here, looking at my files. I know you've been here now.
+title runtime xx3403103!
+echo Windows error, resolving
 COLOR 0a
 pause
